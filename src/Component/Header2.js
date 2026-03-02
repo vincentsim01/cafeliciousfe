26565919d61fed9550453c2804b31252b65966b2
Vincent Sim2 <129670790+vincentsim01@users.noreply.github.com>
2026-03-03 06:36:55 +0800

    feat: add menufull to header

--- src/Component/Header2.js
+++ src/Component/Header2.js
@@ -89,6 +89,7 @@ useEffect(() => {
             <Link to="/" className="menumenu">Home</Link>
             <Link to="/about" className="menumenu">About Us</Link>
             <Link to="/listingFull" className="menumenu">Restaurants</Link>
+            <Link to="/menu" className="menumenu">Menu</Link>
             <Link to="/contact" className="menumenu">Contact Us</Link>
             {/* <button className="buttonld" onClick={blackbg}>Light Dark</button> */}
             </span>
--- src/Component/Routing.js
+++ src/Component/Routing.js
@@ -18,6 +18,7 @@ import Profile from './Profile/profileLogic';
 import QuickFAQ from './FAQ/faq';
 import QuickPromotion from './promotion/promotion';
 import Menu from './menu/menu';
+import MenuFull from './menu/menuFull';
 import QuickThankYou from './thankyou/thankyou';
 
 
@@ -44,6 +45,7 @@ const Routing = () => {
                         <Route path="FAQ" element={<QuickFAQ/>}/>
                         <Route path="promotion" element={<QuickPromotion/>}/>
                         <Route path="menu/:restaurant_id" element={<Menu/>}/>
+                        <Route path="menu" element={<PrivateRoute><MenuFull/></PrivateRoute>}/>
                         <Route path="thankyou" element={<QuickThankYou/>}/>
                         <Route path="profile" element={<PrivateRoute><Profile/></PrivateRoute>}/>
 
