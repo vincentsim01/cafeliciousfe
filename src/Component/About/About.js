dbf56270c578c323abc59c5cb76a3c729837899e
Vincent <vincentkiathadi@gmail.com>
2026-03-19 06:59:50 +0800

    fix: color and alignment about us

--- src/Component/About/About.js
+++ src/Component/About/About.js
@@ -1,5 +1,6 @@
 import React, { useState,useEffect } from 'react';
 import Header2 from '../Header2';
+import './About.css';
 
 const baseUrl = "https://zomato-big-assignment-2-production.up.railway.app";
 const QuickAbout = () => {
@@ -9,17 +10,22 @@ const QuickAbout = () => {
     return(
         <>
         <Header2/>
-        <div id="quickSearch">
-
-            <span className="AboutHeader"><u>About Cafelicious</u></span><br></br>
-            {/* <span className="AboutSubHeader">Find Your Favourite cafe globally</span> */}
+        <div id="AboutContainer">
+            <div id='AboutHeaderContainer'>
+                <span id="AboutHeader"><u>About Cafelicious</u></span><br></br>
+            </div>
+            <br></br>
             <span className="AboutSubHeader">
                 Welcome to Cafelicious, your ultimate destination for a global exploration of the finest cafes! 
                 Cafelicious is not just a directory; it's a curated haven for cafe enthusiasts seeking the crème de la crème of coffee culture worldwide. 
                 Whether you're a seasoned coffee connoisseur or someone just beginning their caffeinated journey, Cafelicious is here to guide you to the best-selling cafes across the globe. 
                 Our platform is dedicated to connecting cafe lovers with the most delightful and sought-after coffee experiences, ensuring that every sip is a memorable one. <br></br>
                 Join us as we embark on a journey to discover the world's most iconic and beloved cafes, making Cafelicious your go-to source for all things coffee and cafe culture. 
-                Cheers to a delicious adventure!</span>
+                Cheers to a delicious adventure!
+            </span>
+
+
+
 
         </div>
         </>
