0e9e02f29595207468cdafdcb421d4e5e030a49c
Vincent <vincentkiathadi@gmail.com>
2026-03-19 11:19:48 +0800

    add: grid about us

--- src/Component/About/About.js
+++ src/Component/About/About.js
@@ -16,29 +16,51 @@ const QuickAbout = () => {
         </section>
         <div id="AboutContainer">
             <div id='AboutHeaderContainer'>
-                <span id="AboutHeader" class='font-bold'><u>About Cafelicious</u></span><br></br>
+                <span id="AboutHeader" class='font-bold font-comfortaa'><u>About Cafelicious</u></span><br></br>
             </div>
             <br></br>
-            <span class="AboutSubHeader p-5">
+            <div class="AboutSubHeader p-5 font-comfortaa">
                 Welcome to Cafelicious, your ultimate destination for a global exploration of the finest cafes! 
                 Cafelicious is not just a directory; it's a curated haven for cafe enthusiasts seeking the crème de la crème of coffee culture worldwide. 
                 Whether you're a seasoned coffee connoisseur or someone just beginning their caffeinated journey, Cafelicious is here to guide you to the best-selling cafes across the globe. 
                 Our platform is dedicated to connecting cafe lovers with the most delightful and sought-after coffee experiences, ensuring that every sip is a memorable one. <br></br>
                 Join us as we embark on a journey to discover the world's most iconic and beloved cafes, making Cafelicious your go-to source for all things coffee and cafe culture. 
                 Cheers to a delicious adventure!
-            </span>
+            </div>
         </div>
-        <section id='aboutceocontainer' class='inline bg-yellow-300 rounded-md m-3 '>
+        <section id='aboutceocontainer' class='inline bg-amber-300 rounded-md m-3 '>
+            <div class='flex justify-center'>
+                <img src='./bordertop.png' class='w-[50%]'></img>
+            </div>
             <div id='aboutceo' class='flex justify-center items-center'>
-                <div id='aboutceoleft' class='inline-block w-[55%] m-3 p-2 font-comfortaa'>
-                    <span class='text-3xl font-bold underline'>Message from our CEO</span>
+
+                <div id='aboutceoleft' class='inline-block md:w-[55%] m-3 p-2 font-comfortaa'>
+                    <span class='text-3xl font-bold underline font-comfortaa'>Message from our CEO</span>
                     <br></br><br></br>
                     Hi Welcome to cafelicious, as the CEO of cafelicious we thank you for your trust in our platform and to help us getting more cafes getting the recognition they deserve.
                     We believe everyone deserves a good cafe experience wherever and whenever they are and cafelicious is here to connect you to your desired cafe.
                 </div>
-                <div id='aboutceoright' class='inline-block w-[20%] m-3 p-2'>
+                <div id='aboutceoright' class='inline-block md:w-[20%] m-3 p-2'>
                     <img src='./ceophoto.png' class='rounded-md'></img>
                 </div>
+            </div>
+            <div class='flex justify-center'>
+                <img src='./border bottom.png' class='w-[50%]'></img>
+            </div>
+          
+        </section>
+        <section>
+            <div class='grid grid-cols-5 grid-rows-3 bg-yellow-200 pt-5'>
+                <div class='col-start-1 col-start-2 border'>
+                    <img src='./cafe1.jpg'></img>
+                </div>
+                <div class='col-start-2 col-start-3'>
+                    <img src='./cafebanner.png' class='w-full'></img>
+                </div>
+                <div class='col-start-4 col-start-5 border'>
+                    <img src='./cafe3.jpg'></img>
+                </div>
+
             </div>
         </section>
         </>
