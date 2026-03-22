1c7d20c0b357d36922be6bac60cc00541566a703
Vincent <vincentkiathadi@gmail.com>
2026-03-22 22:58:35 +0800

    login

--- src/Component/userModify/UserModify.js
+++ src/Component/userModify/UserModify.js
@@ -29,10 +29,12 @@ const UserModify = () => {
         <Header2/>
         {userData.map((user) => {
             return (
-                <div key={user._id}>
-                    <h2>{user.name}</h2>
+                <div key={user._id} className='m-5 p-5 bg-yellow-200 rounded-md hover:shadow-md text-center'>
+                    <h2 className='text-2xl font-bold'>{user.name}</h2>
                     <p>Email: {user.email}</p>
                     <p>Role: {user.role}</p>
+                    <button className='border rounded-md bg-white p-3 m-3 hover:shadow-md '>Edit</button>
+                    <button className='border rounded-md bg-white p-3 m-3 hover:shadow-md '>Delete</button>
                 </div>
             )
 
